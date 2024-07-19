584b879e1f6b0f95f48787c5ba2d0375e3735f78
Arif Muxtarov <departed2015@gmail.com>
2024-07-19 15:51:08 +0400

    Added some files for development purposes

--- bookpublisher/Form1.Designer.cs
+++ bookpublisher/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            faylToolStripMenuItem = new ToolStripMenuItem();
+            opsiyalarToolStripMenuItem = new ToolStripMenuItem();
+            yardımToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { faylToolStripMenuItem, opsiyalarToolStripMenuItem, yardımToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(699, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // faylToolStripMenuItem
+            // 
+            faylToolStripMenuItem.Name = "faylToolStripMenuItem";
+            faylToolStripMenuItem.Size = new Size(40, 20);
+            faylToolStripMenuItem.Text = "Fayl";
+            // 
+            // opsiyalarToolStripMenuItem
+            // 
+            opsiyalarToolStripMenuItem.Name = "opsiyalarToolStripMenuItem";
+            opsiyalarToolStripMenuItem.Size = new Size(68, 20);
+            opsiyalarToolStripMenuItem.Text = "Opsiyalar";
+            // 
+            // yardımToolStripMenuItem
+            // 
+            yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
+            yardımToolStripMenuItem.Size = new Size(56, 20);
+            yardımToolStripMenuItem.Text = "Yardım";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11F);
+            button1.Location = new Point(12, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 54);
+            button1.TabIndex = 1;
+            button1.Text = "Yeni kitab əlavə et";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 11F);
+            button2.Location = new Point(12, 100);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 54);
+            button2.TabIndex = 1;
+            button2.Text = "Kitabı dəyiş";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 11F);
+            button3.Location = new Point(12, 220);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 54);
+            button3.TabIndex = 1;
+            button3.Text = "Kitabı sil";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 11F);
+            button4.Location = new Point(12, 160);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 54);
+            button4.TabIndex = 1;
+            button4.Text = "Tam siyahıya bax";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(130, 40);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(569, 573);
+            dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(699, 613);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "AZTC Kitab Bazası";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem faylToolStripMenuItem;
+        private ToolStripMenuItem opsiyalarToolStripMenuItem;
+        private ToolStripMenuItem yardımToolStripMenuItem;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
     }
 }
